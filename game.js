3fbf5114484aa053707a2ef259cdbade5019b080
Balbuhhha <balbuhhha@gmail.com>
2018-10-04 14:55:09 +0200

    mole styled, renderBoard refactoring done

--- game.js
+++ game.js
@@ -3,6 +3,8 @@
   var height = 5;
   var width = height
 
+  renderBoard(board, height, width)
+
   // Funkcja build zwraca nowy DOM Node o nazwie takiej jak wartość argumentu `name`
   function build(item) {
     return document.createElement(item)
@@ -10,27 +12,31 @@
 
   // Funkcja range zwraca tablicę o takiej długości jak wartość argumentu `size`.
   // Wykorzystamy to do utworzenia x rzędów (height), w których będzie y kolumn (width).
-  function range(size) {
-    var result = [];
-
-    for (let i = 0; i < size; i++) {
-      result.push(i)
+  
+  function renderBoard(board, height, width){
+    function range(size) {
+      var result = [];
+  
+      for (let i = 0; i < size; i++) {
+        result.push(i)
+      }
+  
+      return result
     }
-
-    return result
-  }
-
-  range(height).forEach(function () {
-    var row = build('div');
-    row.classList.add('row');
-
-    range(width).forEach(function () {
-      var cell = build('div');
-      cell.classList.add('cell');
-      row.appendChild(cell)
+  
+    range(height).forEach(function () {
+      var row = build('div');
+      row.classList.add('row');
+  
+      range(width).forEach(function () {
+        var cell = build('div');
+        cell.classList.add('cell');
+        row.appendChild(cell)
+      })
+      board.appendChild(row)
     })
-    board.appendChild(row)
-  })
+  }
+  
 
 
 
