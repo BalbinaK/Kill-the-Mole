38db6e719faa6f73fa36da82920daae3bd38b89b
Balbuhhha <balbuhhha@gmail.com>
2018-10-04 14:28:51 +0200

    f build(item) added

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 (function () {
+var board = document.querySelector('#app');
+
+// Funkcja build zwraca nowy DOM Node o nazwie takiej jak wartość argumentu `name`
+function build (item) {
+  return document.createElement(item)
+}
 
 
-  
+
 })()
